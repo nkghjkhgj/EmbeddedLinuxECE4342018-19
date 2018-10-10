2f312974d0cf6dd48006262c04166c172adf41f6
Demo User <debian@beaglebone.localdomain>
2018-10-10 02:46:58 +0000

    recover from a data loss

---
+++ hw07/Readme.md
@@ -0,0 +1,7 @@
+1.BLynk
+In this homework I first downloaded Blynk library to the bone and Blynk software to my smartphone to try out the examples, which toggles both physical led connected to a gpio pin to the bone and a virtual led on the phone.
+Then I implemented a slider to to control the brightness of led connected to the bone.
+The scripts can be found in the folder.
+
+2. Project
+Kuangyi and I created a new page called Project-Music Synthesizer and edited the executive summary.
---
+++ hw07/leds.js
@@ -0,0 +1,35 @@
+var Blynk = require('/usr/lib/node_modules/blynk-library');
+const b = require('bonescript');
+const util = require('util');
+
+const LED0 = 'GREEN';
+const button = 'GP1_3';
+b.pinMode(LED0, b.OUTPUT);
+b.pinMode(button, b.INPUT);
+
+const AUTH = '304f140207334cf8ab130bb9df732ae0';
+
+var blynk = new Blynk.Blynk(AUTH);
+
+var v0 = new blynk.VirtualPin(0);
+var v10 = new blynk.WidgetLED(10);
+// console.log(util.inspect(v1));
+// var v9 = new blynk.VirtualPin(9);
+
+v0.on('write', function(param) {
+    console.log('V0:', param[0]);
+    b.digitalWrite(LED0, param[0]);
+});
+
+v10.setValue(0);    // Initiallly off
+
+// v9.on('read', function() {
+//     v9.write(new Date().getSeconds());
+// });
+
+b.attachInterrupt(button, toggle, b.CHANGE);
+
+function toggle(x) {
+    console.log("V1: ", x.value);
+    x.value ? v10.turnOff() : v10.turnOn();
+}
